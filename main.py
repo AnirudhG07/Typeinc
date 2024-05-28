2201a8da8ad6d7600397fdf98696b7305942c0ab
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-05-29 00:45:17 +0530

    removed faker, temp removed argparsing, results functioning well

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import curses, string
-import time
+import curses
 import random
 import argparse
-from faker import Faker 
+import time
 
-from .text_gen import text_gen
-from .result import result
+from text_gen import text_gen
+from result import result
 
 def ui():
     """
@@ -14,7 +13,7 @@ def ui():
     pass
 
 
-def main(stdscr):
+def main(stdscr, numwords, alphanumeric):
     """
     The main function will have the following steps:
     1) Handle the flags, parameters
@@ -23,16 +22,7 @@ def main(stdscr):
     4) Ask the user if they want to continue
 
     """
-    # Handle the flags, parameters
-    parser = argparse.ArgumentParser(description='Typing speed test')
-    parser.add_argument('-t', '--time', type=int, help='Time limit for the test')
-    parser.add_argument('-n', '--number', type=int, help='Number of words to type')
-    parser.add_argument('-a', '--alphanumeric', type= int, help='Include numbers and special characters')
-
-    args = parser.parse_args()
-
-    if args.n:
-        text = text_gen(args.n, args.a)
+    test_text = text_gen(numwords,alphanumeric)
     # Clear screen
     stdscr.clear()
 
@@ -40,11 +30,14 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
-    words = text.split()
-    selected_words = random.sample(words, 20)
+    words = test_text.split()
+
+    errors_pos = set()
+    total_chars = sum(len(word) for word in words)
+    total_words = len(words)
 
     # Arrange words into lines of maximum 7 words each
-    lines = [' '.join(selected_words[i:i+7]) + ' ' for i in range(0, len(selected_words), 7)]
+    lines = [' '.join(words[i:i+7]) + ' ' for i in range(0, len(words), 7)]
     for i, line in enumerate(lines):
         stdscr.addstr(i, 0, line, curses.color_pair(0))
 
@@ -53,6 +46,7 @@ def main(stdscr):
 
     i = 0
     j = 0
+    start_time = time.time()
     while i < len(lines):
         line = lines[i]
         while j < len(line):
@@ -64,20 +58,32 @@ def main(stdscr):
                 elif i > 0:
                     i -= 1
                     j = len(lines[i]) - 1
-                stdscr.addstr(i, j, line[j], curses.color_pair(0))  # Replace the character with a space
                 stdscr.move(i, j)  # Move the cursor back
             else:
                 if user_input == char:
                     stdscr.addstr(i, j, char, curses.color_pair(1))
                 else:
-                    stdscr.addstr(i, j, char, curses.color_pair(2))
+                    stdscr.addstr(i, j, char, curses.color_pair(2)) # Red color for wrong characters
+                    errors_pos.add(10*i+j)
                 j += 1
 
             stdscr.refresh()
 
         i += 1
         j = 0
+    end_time = time.time()
 
-    stdscr.getch()
+    # Show the results
+    retry_flag=result(stdscr, total_words, total_chars, int(end_time-start_time), len(errors_pos))
+    if retry_flag== True:
+        curses.endwin()
+        numwords=int(input("Enter the number of words you want to type: "))
+        alphanumeric=int(input("Do you want to add special characters? "))
+        curses.wrapper(main, numwords, alphanumeric)
+    else:
+        exit(0)
 
-curses.wrapper(main)
+if __name__ == '__main__':
+    numwords=int(input("Enter the number of words you want to type: "))
+    alphanumeric=int(input("Do you want to add special characters? "))
+    curses.wrapper(main, numwords, alphanumeric)
